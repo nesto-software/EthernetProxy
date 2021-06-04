8d6c356b6f2495fdb1fbe1365722dacd24fdb863
Martin Löper <martin.loeper@nesto-software.de>
2021-06-05 00:53:18 +0200

    fix: remove package versions

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # libpcap
-sudo apt-get install -y libpcap0.8=1.8.1-6
+sudo apt-get install -y libpcap0.8
 sudo ln -s /usr/lib/arm-linux-gnueabihf/libpcap.so.0.8 /usr/lib/arm-linux-gnueabihf/libpcap.so.1
 
 # expecting openssl version 1.1.1d
-sudo apt install -y openssl=1.1.1d-0+deb10u6+rpt1
+sudo apt install -y openssl
 
 # libzmq
-sudo apt install -y libzmq3-dev=4.3.1-4+deb10u2
+sudo apt install -y libzmq3-dev
